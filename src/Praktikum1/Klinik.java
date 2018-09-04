a48c9d6d835e407bee881e7f5b9f5fd35311d639
Satrio <Satrio@SatrioDanur>
2018-09-04 22:52:03 +0700

    Praktikum 1

--- src/Praktikum1/Klinik.java
+++ src/Praktikum1/Klinik.java
@@ -10,21 +10,21 @@ package Praktikum1;
  * @author Satrio
  */
 public class Klinik {
-    private String idKlinik, nama;
+    private String idKlinik, nama;//Pendeklarasian variabel bertipe data String
 
-    public String getIdKlinik() {
+    public String getIdKlinik() {//membuat method baru yang akan mereturn variabel bertipe data String
         return idKlinik;
     }
 
-    public void setIdKlinik(String idKlinik) {
+    public void setIdKlinik(String idKlinik) {//membuat method baru yang digunakan untuk memasukkan data dari parameter idKlinik kedalam variabel idKlinik
         this.idKlinik = idKlinik;
     }
 
-    public String getNama() {
+    public String getNama() {//membuat method baru yang akan mereturn variabel bertipe data String
         return nama;
     }
 
-    public void setNama(String nama) {
+    public void setNama(String nama) { //membuat method baru yang digunakan untuk memasukkan data dari parameter nama kedalam variabel nama
         this.nama = nama;
     }
 }
