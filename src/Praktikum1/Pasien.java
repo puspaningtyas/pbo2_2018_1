bd742675c207f3f160abaae6f8deec8240414270
Satrio <Satrio@SatrioDanur>
2018-09-04 22:52:08 +0700

    Praktikum 1

--- src/Praktikum1/Pasien.java
+++ src/Praktikum1/Pasien.java
@@ -10,83 +10,83 @@ package Praktikum1;
  * @author Satrio
  */
 public class Pasien {
-    private String noRekamMedis, nama, alamat, tempatLahir;
-    private int tanggalLahir, bulanLahir, tahunLahir;
+    private String noRekamMedis, nama, alamat, tempatLahir; //Pendeklarasian variabel bertipe data String
+    private int tanggalLahir, bulanLahir, tahunLahir; //Pendeklarasian variabel bertipe data integer
     
-    public Pasien(){
+    public Pasien(){  //menambahkan konstruktor, Konsktruktor adalah method yang memiliki nama yang sama dengan nama kelas
         
     }
     
 
-    public String getNoRekamMedis() {
+    public String getNoRekamMedis() { //membuat method bernama getNoRekamedis yang akan mereturn variabel noRekamMedis yang bertipe data String
         return noRekamMedis;
     }
 
-    public void setNoRekamMedis(String noRekamMedis) throws NumberFormatException {
-        if (noRekamMedis.toCharArray().length>=6) {
+    public void setNoRekamMedis(String noRekamMedis) throws NumberFormatException { //membuat method void dengan menambahkan fungsi throws NumberFormatException
+        if (noRekamMedis.toCharArray().length>=6) { //Method ini menyeleksi array baru, yang panjangnya harus sesuai dengan yang ditentukan sebelumnya
             this.noRekamMedis = noRekamMedis;
         }else{
-                throw new NumberFormatException("Nomor Rekam Medis Salah");
+                throw new NumberFormatException("Nomor Rekam Medis Salah"); //NumberFormatException berfungsi untuk memberitahu kesalahan user dalam format penulisan angka.
                 }
     }
 
-    public String getNama() {
+    public String getNama() { //membuat method baru yang akan mereturn variabel bertipe data String
         return nama;
     }
 
-    public void setNama(String nama) {
+    public void setNama(String nama) { //membuat method baru yang digunakan untuk memasukkan data dari parameter nama kedalam variabel nama
         this.nama = nama;
     }
 
-    public String getAlamat() {
+    public String getAlamat() { //membuat method baru yang akan mereturn variabel bertipe data String
         return alamat;
     }
 
-    public void setAlamat(String alamat) {
+    public void setAlamat(String alamat) { //membuat method baru yang digunakan untuk memasukkan data dari parameter alamat kedalam variabel alamat
         this.alamat = alamat;
     }
 
-    public String getTempatLahir() {
+    public String getTempatLahir() {//membuat method baru yang akan mereturn variabel bertipe data String
         return tempatLahir;
     }
 
-    public void setTempatLahir(String tempatLahir) {
+    public void setTempatLahir(String tempatLahir) { //membuat method baru yang digunakan untuk memasukkan data dari parameter tempatLahir kedalam variabel tempatLahir
         this.tempatLahir = tempatLahir;
     }
 
-    public int getTanggalLahir() {
+    public int getTanggalLahir() {//membuat method baru yang akan mereturn variabel bertipe data integer
         return tanggalLahir;
     }
 
-    public void setTanggalLahir(int tanggalLahir) throws Exception {
+    public void setTanggalLahir(int tanggalLahir) throws Exception { //membuat method void dengan menambahkan fungsi throws NumberFormatException
         if (tanggalLahir > 0 && tanggalLahir < 31) {
             this.tanggalLahir = tanggalLahir;
         } else {
-            throw new Exception(" Tanggal Lahir Anda Tidak Valid ");
+            throw new Exception(" Tanggal Lahir Anda Tidak Valid "); //berfungsi untuk memberitahu kesalahan user dalam menginputkan tanggal lahir.
         }
     }
 
-    public int getBulanLahir() {
+    public int getBulanLahir() {//membuat method baru yang akan mereturn variabel bertipe data integer
         return bulanLahir;
     }
 
-    public void setBulanLahir(int bulanLahir) throws Exception {
+    public void setBulanLahir(int bulanLahir) throws Exception { //membuat method void dengan menambahkan fungsi throws NumberFormatException
         if (bulanLahir > 0 && bulanLahir < 13) {
             this.bulanLahir = bulanLahir;
         } else {
-            throw new Exception(" Bulan Lahir Anda Tidak Valid ");
+            throw new Exception(" Bulan Lahir Anda Tidak Valid "); //berfungsi untuk memberitahu kesalahan user dalam menginputkan bulanLahir.
         }
     }
 
-    public int getTahunLahir() {
+    public int getTahunLahir() {//membuat method baru yang akan mereturn variabel bertipe data integer
         return tahunLahir;
     }
 
-    public void setTahunLahir(int tahunLahir) throws Exception {
+    public void setTahunLahir(int tahunLahir) throws Exception {//membuat method void dengan menambahkan fungsi throws NumberFormatException
         if (tahunLahir > 0) {
             this.tahunLahir = tahunLahir;
         } else {
-            throw new Exception(" Tahun Lahir Anda Tidak Valid ");
+            throw new Exception(" Tahun Lahir Anda Tidak Valid "); //berfungsi untuk memberitahu kesalahan user dalam menginputkan tahunLahir.
         }
     }
 }
