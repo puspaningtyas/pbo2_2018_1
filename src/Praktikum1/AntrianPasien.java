915662aea35f2d025018b225ad0cbf1b0df972d1
Satrio <Satrio@SatrioDanur>
2018-09-04 22:27:57 +0700

    ver 0.5

--- src/Praktikum1/AntrianPasien.java
+++ src/Praktikum1/AntrianPasien.java
@@ -10,23 +10,23 @@ package Praktikum1;
  * @author Satrio
  */
 public class AntrianPasien {
-    private int tanggalAntrian, bulanAntrian, tahunAntrian;
-    private Klinik klinik;
-    private Pasien daftarPasien[];
+    private int tanggalAntrian, bulanAntrian, tahunAntrian; //Pendeklarasian variabel bertipe data integer
+    private Klinik klinik; //Pendeklarasian variabel bertipe data Klinik 
+    private Pasien daftarPasien[]; // pendeklarasian array bertipe data pasien 
 
-    public Klinik getKlinik() {
+    public Klinik getKlinik() {//membuat method baru yang akan mereturn variabel bertipe data klinik
         return klinik;
     }
 
-    public void setKlinik(Klinik klinik) {
+    public void setKlinik(Klinik klinik) {//membuat method baru yang digunakan untuk memasukkan data dari parameter klinik kedalam variabel klinik
         this.klinik = klinik;
     }
 
-    public Pasien[] getDaftarPasien() {
+    public Pasien[] getDaftarPasien() {//membuat method baru yang akan mereturn variabel bertipe data aray pasien
         return daftarPasien;
     }
 
-    public void setDaftarPasien(Pasien[] daftarPasien) {
+    public void setDaftarPasien(Pasien[] daftarPasien) {//membuat method baru yang digunakan untuk memasukkan data dari parameter daftarPasien kedalam variabel daftarPasien
         this.daftarPasien = daftarPasien;
     }
 }
