39b09d0196ca888221be238b666d22d85a9ab707
admin <admin@8b>
2018-09-05 10:50:47 +0700

    ver0.5

---
+++ src/TestView/DaftarAntrianDialog.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestView;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author admin
+ */
+public class DaftarAntrianDialog extends JDialog{
+    private JLabel judulLabel, namaLabel;
+    private JTextField namaText;
+    private JButton saveButton;
+    
+    public static void init(){
+        
+    }
+}
