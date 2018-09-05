0e9bc3516d3399ab0753078b0cb3bafa1611dcc3
admin <admin@dosenb>
2018-09-05 10:52:00 +0700

    tambah test supervisor

---
+++ src/testSuperVisor/Testpasien.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testSuperVisor;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Pasien;
+
+/**
+ *
+ * @author admin
+ */
+public class Testpasien {
+    public static void main(String[] args) {
+        Pasien test = new Pasien();
+        test.setNama("Puspa");
+        try {
+            test.setBulanLahir(12);
+            System.out.println("bulan = "+test.getBulanLahir());
+        } catch (Exception ex) {
+            Logger.getLogger(Testpasien.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+ 
+}
